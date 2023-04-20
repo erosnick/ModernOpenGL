7c8c45b6a205530fa209150d4f7295e2070e8ee0
Princerin <erosnick@gmail.com>
2023-04-20 21:40:48 +0800

    1.Output OpenGL debug message according to level. 2.Add glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS) to get the correct call stack when an exception occurs.

--- Src/Utils/glDebug.cpp
+++ Src/Utils/glDebug.cpp
@@ -3,6 +3,16 @@
 
 #include "Log.h"
 
+#ifdef ARIA_CORE_DEBUG
+#define ARIA_CORE_ENABLE_ASSERTS
+#endif
+
+#ifdef ARIA_CORE_ENABLE_ASSERTS
+#define ARIA_CORE_ASSERT(x, ...) { if(!(x)) { ARIA_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+#define ARIA_CORE_ASSERT(x, ...)
+#endif
+
 void APIENTRY glDebugOutput(GLenum source,
 	GLenum type,
 	GLuint id,
@@ -14,36 +24,36 @@ void APIENTRY glDebugOutput(GLenum source,
 	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
 
 	ARIA_CORE_TRACE("---------------");
-	ARIA_CORE_TRACE("Debug message ({0}):{1}", id, message);
+	ARIA_CORE_ERROR("Debug message ({0}):{1}", id, message);
 
 	switch (source)
 	{
-	case GL_DEBUG_SOURCE_API:             ARIA_CORE_TRACE("Source: API"); break;
-	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   ARIA_CORE_TRACE("Source: Window System"); break;
-	case GL_DEBUG_SOURCE_SHADER_COMPILER: ARIA_CORE_TRACE("Source: Shader Compiler"); break;
-	case GL_DEBUG_SOURCE_THIRD_PARTY:     ARIA_CORE_TRACE("Source: Third Party"); break;
-	case GL_DEBUG_SOURCE_APPLICATION:     ARIA_CORE_TRACE("Source: Application"); break;
-	case GL_DEBUG_SOURCE_OTHER:           ARIA_CORE_TRACE("Source: Other"); break;
+	case GL_DEBUG_SOURCE_API:             ARIA_CORE_ERROR("Source: API"); break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   ARIA_CORE_ERROR("Source: Window System"); break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: ARIA_CORE_ERROR("Source: Shader Compiler"); break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     ARIA_CORE_ERROR("Source: Third Party"); break;
+	case GL_DEBUG_SOURCE_APPLICATION:     ARIA_CORE_ERROR("Source: Application"); break;
+	case GL_DEBUG_SOURCE_OTHER:           ARIA_CORE_ERROR("Source: Other"); break;
 	}
 
 	switch (type)
 	{
-	case GL_DEBUG_TYPE_ERROR:               ARIA_CORE_TRACE("Type: Error"); break;
-	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: ARIA_CORE_TRACE("Type: Deprecated Behaviour"); break;
-	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  ARIA_CORE_TRACE("Type: Undefined Behaviour"); break;
-	case GL_DEBUG_TYPE_PORTABILITY:         ARIA_CORE_TRACE("Type: Portability"); break;
-	case GL_DEBUG_TYPE_PERFORMANCE:         ARIA_CORE_TRACE("Type: Performance"); break;
-	case GL_DEBUG_TYPE_MARKER:              ARIA_CORE_TRACE("Type: Marker"); break;
-	case GL_DEBUG_TYPE_PUSH_GROUP:          ARIA_CORE_TRACE("Type: Push Group"); break;
-	case GL_DEBUG_TYPE_POP_GROUP:           ARIA_CORE_TRACE("Type: Pop Group"); break;
-	case GL_DEBUG_TYPE_OTHER:               ARIA_CORE_TRACE("Type: Other"); break;
+	case GL_DEBUG_TYPE_ERROR:               ARIA_CORE_ERROR("Type: Error"); break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: ARIA_CORE_ERROR("Type: Deprecated Behaviour"); break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  ARIA_CORE_ERROR("Type: Undefined Behaviour"); break;
+	case GL_DEBUG_TYPE_PORTABILITY:         ARIA_CORE_ERROR("Type: Portability"); break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         ARIA_CORE_ERROR("Type: Performance"); break;
+	case GL_DEBUG_TYPE_MARKER:              ARIA_CORE_ERROR("Type: Marker"); break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          ARIA_CORE_ERROR("Type: Push Group"); break;
+	case GL_DEBUG_TYPE_POP_GROUP:           ARIA_CORE_ERROR("Type: Pop Group"); break;
+	case GL_DEBUG_TYPE_OTHER:               ARIA_CORE_ERROR("Type: Other"); break;
 	}
 
 	switch (severity)
 	{
-	case GL_DEBUG_SEVERITY_HIGH:         ARIA_CORE_TRACE("Severity: high"); break;
-	case GL_DEBUG_SEVERITY_MEDIUM:       ARIA_CORE_TRACE("Severity: medium"); break;
-	case GL_DEBUG_SEVERITY_LOW:          ARIA_CORE_TRACE("Severity: low"); break;
+	case GL_DEBUG_SEVERITY_HIGH:         ARIA_CORE_ERROR("Severity: high"); ARIA_CORE_ASSERT(false, "GL_DEBUG_SEVERITY_HIGH"); break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       ARIA_CORE_WARN("Severity: medium"); break;
+	case GL_DEBUG_SEVERITY_LOW:          ARIA_CORE_INFO("Severity: low"); break;
 	case GL_DEBUG_SEVERITY_NOTIFICATION: ARIA_CORE_TRACE("Severity: notification"); break;
 	}
 }
